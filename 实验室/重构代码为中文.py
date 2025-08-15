399147f7e84220995a9758bd4bc7ae18f7477053
LZY <2217445143@qq.com>
2025-08-15 14:50:21 +0800

    备份

---
+++ 实验室/重构代码为中文.py
@@ -0,0 +1,20 @@
+import libcst as cst
+
+class RenameVisitor(cst.CSTTransformer):
+    def leave_Name(self, original_node, updated_node):
+        if original_node.value == "打印":
+            return updated_node.with_changes(value="print")
+        return updated_node
+
+code = """
+def 打印(x):
+    return x
+
+打印(123)
+"""
+
+# 解析 + 替换
+module = cst.parse_module(code)
+new_module = module.visit(RenameVisitor())
+
+print(new_module.code)
